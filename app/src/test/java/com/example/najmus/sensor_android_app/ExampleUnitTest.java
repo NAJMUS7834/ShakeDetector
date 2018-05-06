76bb96e0f98aeda45e37e8a5b5f8c6fbcc2d44ea
Najmus_Seemab <najmus.seemab@gmail.com>
2018-05-06 19:51:08 +0530

    Update ExampleUnitTest.java

--- app/src/test/java/com/example/najmus/sensor_android_app/ExampleUnitTest.java
+++ app/src/test/java/com/example/najmus/sensor_android_app/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package com.example.najmus.sensor_android_app;
-
+/*====================================
+    Author : NAJMUS SEEMAB
+======================================*/
 import org.junit.Test;
 
 import static org.junit.Assert.*;
